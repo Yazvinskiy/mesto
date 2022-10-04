8ca515ab3228d80f2ee1760fbdbbb3a8fc418c01
Yazvinskiy <yazvinskiy94@gmail.com>
2022-10-04 20:20:59 +0300

    2

--- scripts/sripts.js
+++ scripts/sripts.js
@@ -4,22 +4,28 @@ const openModalBtn = document.querySelector(".profile__edit-button");
 const modal = document.querySelector(".modal");
 const modalConteiner = modal.querySelector(".modal__conteiner");
 const modalCloseBtn = modal.querySelector(".modal__btn-close");
-const form = modal.querySelector(".modal__form");
-const inputName = modal.querySelector(".modal__input_name");
-const inputProfession = modal.querySelector(".modal__input_profession");
+const form = document.querySelector(".modal__form");
+const inputName = document.querySelector(".modal__input_name");
+const inputProfession = document.querySelector(".modal__input_profession");
+const profileName = document.querySelector(".profile__title");
+const profileProfession = document.querySelector(".profile__subtitle");
 
-openModalBtn.addEventListener("click", (event) => {
+openModalBtn.addEventListener("click", function () {
     modal.classList.add(MODAL_ACTIVE_CLASS);
+    inputName.value = profileName.textContent;
+    inputProfession.value = profileProfession.textContent;
 });
 
-modal.addEventListener("click", (event) =>{
-if(!modalConteiner.contains(event.target) || event.target === modalCloseBtn){
-    modal.classList.remove(MODAL_ACTIVE_CLASS);
+const handleModalClick = (event) => {
+    if (!modalConteiner.contains(event.target) || event.target === modalCloseBtn) {
+        modal.classList.remove(MODAL_ACTIVE_CLASS);
+    }
 }
-});
 
-form.addEventListener("sudmit", (event) => {
-    event.preventDefault();
-   form.querySelector(".form__input_name");
-   form.querySelector(".modal__input_profession");
-});
+modal.addEventListener('click', handleModalClick);
+
+ function handleModalSubmit(){
+    
+    profileName.textContent = inputName.value;
+    profileProfession.textContent = inputProfession.value;
+}
