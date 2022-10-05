cf70f068098718709284a99eb447e1851210a6f7
Yazvinskiy <yazvinskiy94@gmail.com>
2022-10-06 00:16:37 +0300

    correct mistakes

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const POPUP_OPENED_CLASS = 'popup_opened';
+const openPopupBtn = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const popupConteiner = popup.querySelector(".popup__conteiner");
+const popupCloseBtn = popup.querySelector(".popup__btn-close");
+const form = document.querySelector(".popup__form");
+const inputName = document.querySelector(".popup__input_user_name");
+const inputProfession = document.querySelector(".popup__input_user_profession");
+const profileName = document.querySelector(".profile__title");
+const profileProfession = document.querySelector(".profile__subtitle");
+
+function openPopup() {
+    popup.classList.add(POPUP_OPENED_CLASS);
+    inputName.value = profileName.textContent;
+    inputProfession.value = profileProfession.textContent;
+};
+
+function handlePopupClick(event) {
+    if (!popupConteiner.contains(event.target) || event.target === popupCloseBtn) {
+        popup.classList.remove(POPUP_OPENED_CLASS);
+    }
+};
+
+function popupSubmitHandler(evt) {
+    evt.preventDefault(evt);
+    profileName.textContent = inputName.value;
+    profileProfession.textContent = inputProfession.value;
+    if (form.contains(evt.target)) {
+        popup.classList.remove(POPUP_OPENED_CLASS);
+    }
+};
+
+openPopupBtn.addEventListener("click",openPopup);
+popup.addEventListener('click', handlePopupClick);
+form.addEventListener('submit', popupSubmitHandler); 
